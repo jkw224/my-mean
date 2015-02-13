3218ac37e4ffd622747721bea047b6997814408f
Jonathan Wood <jonathankimballwood@gmail.com>
2015-02-12 19:29:04 -0700

    quiz-1 done foo

---
+++ quizzes/quiz-1/5-item-array.js
@@ -0,0 +1,10 @@
+var fiveItemArray = ["my", "name", "is", "jon", 28];
+
+function arrayPrinter() {
+	for (var i = 0; i < fiveItemArray.length; i++) {
+		var answer = console.log(fiveItemArray[i])
+	};
+	return answer;	
+};
+
+arrayPrinter();
